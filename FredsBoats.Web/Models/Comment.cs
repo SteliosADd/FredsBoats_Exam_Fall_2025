bea59b7a01d22b68f56fb9bb6c03e3830be346e8
SteliosADd <173936279+SteliosADd@users.noreply.github.com>
2026-01-30 09:28:13 +0000

    Completed Lab

---
+++ FredsBoats.Web/Models/Comment.cs
@@ -0,0 +1,11 @@
+public class Comment
+ {
+
+public int CommentId {get; set;}
+public string Content {get; set;}
+public string Author {get; set;}
+public DateTime CreatedAt {get; set;}
+public int BoatId {get; set;}
+public Boat Boat {get; set;}
+
+}
